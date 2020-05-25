7420c38076bba7734b3b0103f6abf6b09c5fbcd2
Daniel Velasquez <42074978+dvlsqz@users.noreply.github.com>
2020-05-24 21:23:04 -0600

    24052020
    Database

--- DATABASE/cheques.sql
+++ DATABASE/cheques.sql
@@ -1,88 +1,330 @@
--- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
+CREATE DATABASE  IF NOT EXISTS `db_cheques` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish2_ci */;
+USE `db_cheques`;
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 25-05-2020 a las 05:14:13
--- Versión del servidor: 10.4.11-MariaDB
--- Versión de PHP: 7.4.4
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: 127.0.0.1    Database: db_cheques
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.11-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Base de datos: `db_cheques`
+-- Table structure for table `bancos`
 --
 
--- --------------------------------------------------------
+DROP TABLE IF EXISTS `bancos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `bancos` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `contacto` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `numero` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `condicion` tinyint(1) NOT NULL DEFAULT 1,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estructura de tabla para la tabla `cheques`
+-- Dumping data for table `bancos`
 --
 
+LOCK TABLES `bancos` WRITE;
+/*!40000 ALTER TABLE `bancos` DISABLE KEYS */;
+INSERT INTO `bancos` VALUES (1,'Banrural S.A','Javier Perez','46598321',1,'2020-05-25 04:34:30','2020-05-25 04:43:24'),(2,'Banco Industrial','Luisa Gomez','56875213',1,'2020-05-25 04:36:14','2020-05-25 04:36:14');
+/*!40000 ALTER TABLE `bancos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `chequeras`
+--
+
+DROP TABLE IF EXISTS `chequeras`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `chequeras` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `chequera` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `observaciones` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ch_inicio` decimal(11,2) NOT NULL,
+  `ch_fin` decimal(11,2) NOT NULL,
+  `condicion` tinyint(1) NOT NULL DEFAULT 1,
+  `idcuenta` int(10) unsigned NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `chequeras_chequera_unique` (`chequera`),
+  KEY `chequeras_idcuenta_foreign` (`idcuenta`),
+  CONSTRAINT `chequeras_idcuenta_foreign` FOREIGN KEY (`idcuenta`) REFERENCES `cuentas` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `chequeras`
+--
+
+LOCK TABLES `chequeras` WRITE;
+/*!40000 ALTER TABLE `chequeras` DISABLE KEYS */;
+INSERT INTO `chequeras` VALUES (1,'1','Chequera para proveedores especiales',1.00,20.00,0,1,'2020-05-25 06:50:53','2020-05-25 06:52:46');
+/*!40000 ALTER TABLE `chequeras` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cheques`
+--
+
+DROP TABLE IF EXISTS `cheques`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `cheques` (
-  `id` int(10) UNSIGNED NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `no_cheque` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `monto` decimal(11,2) NOT NULL,
   `monto_letras` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
   `lugar` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
   `fecha` date NOT NULL,
   `condicion` tinyint(1) NOT NULL DEFAULT 1,
-  `idchequera` int(10) UNSIGNED NOT NULL,
-  `idproveedor` int(10) UNSIGNED NOT NULL,
+  `idchequera` int(10) unsigned NOT NULL,
+  `idproveedor` int(10) unsigned NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `cheques_no_cheque_unique` (`no_cheque`),
+  KEY `cheques_idchequera_foreign` (`idchequera`),
+  KEY `cheques_idproveedor_foreign` (`idproveedor`),
+  CONSTRAINT `cheques_idchequera_foreign` FOREIGN KEY (`idchequera`) REFERENCES `chequeras` (`id`),
+  CONSTRAINT `cheques_idproveedor_foreign` FOREIGN KEY (`idproveedor`) REFERENCES `proveedores` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cheques`
+--
+
+LOCK TABLES `cheques` WRITE;
+/*!40000 ALTER TABLE `cheques` DISABLE KEYS */;
+INSERT INTO `cheques` VALUES (1,'1',125.50,'Ciento veinticinco con cincuenta centavos','San Marcos','2020-05-24',1,1,1,'2020-05-25 09:10:53','2020-05-25 09:12:53');
+/*!40000 ALTER TABLE `cheques` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cuentas`
+--
+
+DROP TABLE IF EXISTS `cuentas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cuentas` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `no_cuenta` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tipo` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `saldo` decimal(11,2) NOT NULL,
+  `condicion` tinyint(1) NOT NULL DEFAULT 1,
+  `idbanco` int(10) unsigned NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `cuentas_no_cuenta_unique` (`no_cuenta`),
+  KEY `cuentas_idbanco_foreign` (`idbanco`),
+  CONSTRAINT `cuentas_idbanco_foreign` FOREIGN KEY (`idbanco`) REFERENCES `bancos` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cuentas`
+--
+
+LOCK TABLES `cuentas` WRITE;
+/*!40000 ALTER TABLE `cuentas` DISABLE KEYS */;
+INSERT INTO `cuentas` VALUES (1,'3092156127','Monetaria',10000.00,1,1,'2020-05-25 06:18:10','2020-05-25 06:18:10');
+/*!40000 ALTER TABLE `cuentas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `failed_jobs`
+--
+
+DROP TABLE IF EXISTS `failed_jobs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Volcado de datos para la tabla `cheques`
+-- Dumping data for table `failed_jobs`
 --
 
-INSERT INTO `cheques` (`id`, `no_cheque`, `monto`, `monto_letras`, `lugar`, `fecha`, `condicion`, `idchequera`, `idproveedor`, `created_at`, `updated_at`) VALUES
-(1, '1', '125.50', 'Ciento veinticinco con cincuenta centavos', 'San Marcos', '2020-05-24', 1, 1, 1, '2020-05-25 09:10:53', '2020-05-25 09:12:53');
+LOCK TABLES `failed_jobs` WRITE;
+/*!40000 ALTER TABLE `failed_jobs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `failed_jobs` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Índices para tablas volcadas
+-- Table structure for table `migrations`
 --
 
---
--- Indices de la tabla `cheques`
---
-ALTER TABLE `cheques`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `cheques_no_cheque_unique` (`no_cheque`),
-  ADD KEY `cheques_idchequera_foreign` (`idchequera`),
-  ADD KEY `cheques_idproveedor_foreign` (`idproveedor`);
+DROP TABLE IF EXISTS `migrations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `migrations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- AUTO_INCREMENT de las tablas volcadas
+-- Dumping data for table `migrations`
 --
 
---
--- AUTO_INCREMENT de la tabla `cheques`
---
-ALTER TABLE `cheques`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+LOCK TABLES `migrations` WRITE;
+/*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
+INSERT INTO `migrations` VALUES (2,'2014_10_12_100000_create_password_resets_table',1),(3,'2019_08_19_000000_create_failed_jobs_table',1),(4,'2020_05_24_214908_create_roles_table',2),(5,'2020_05_24_215100_create_users_table',3),(6,'2020_05_24_220637_create_bancos_table',4),(8,'2020_05_24_231353_create_cuentas_table',5),(9,'2020_05_25_000039_create_chequeras_table',5),(11,'2020_05_25_022113_create_proveedores_table',6),(13,'2020_05_25_022910_create_cheques_table',7);
+/*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Restricciones para tablas volcadas
+-- Table structure for table `password_resets`
 --
 
---
--- Filtros para la tabla `cheques`
---
-ALTER TABLE `cheques`
-  ADD CONSTRAINT `cheques_idchequera_foreign` FOREIGN KEY (`idchequera`) REFERENCES `chequeras` (`id`),
-  ADD CONSTRAINT `cheques_idproveedor_foreign` FOREIGN KEY (`idproveedor`) REFERENCES `proveedores` (`id`);
-COMMIT;
+DROP TABLE IF EXISTS `password_resets`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `password_resets` (
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  KEY `password_resets_email_index` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `password_resets`
+--
+
+LOCK TABLES `password_resets` WRITE;
+/*!40000 ALTER TABLE `password_resets` DISABLE KEYS */;
+/*!40000 ALTER TABLE `password_resets` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `proveedores`
+--
+
+DROP TABLE IF EXISTS `proveedores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `proveedores` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `numero` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `condicion` tinyint(1) NOT NULL DEFAULT 1,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `proveedores`
+--
+
+LOCK TABLES `proveedores` WRITE;
+/*!40000 ALTER TABLE `proveedores` DISABLE KEYS */;
+INSERT INTO `proveedores` VALUES (1,'Inversiones GT S.A','79568432',1,'2020-05-25 08:23:15','2020-05-25 08:24:33');
+/*!40000 ALTER TABLE `proveedores` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `roles` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `descripcion` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `condicion` tinyint(1) NOT NULL DEFAULT 1,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `roles_nombre_unique` (`nombre`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES (1,'Administrador','Administrador del Sistema',1);
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `usuario` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `condicion` tinyint(1) NOT NULL DEFAULT 1,
+  `monto_min` decimal(11,2) NOT NULL,
+  `monto_max` decimal(11,2) NOT NULL,
+  `idrol` int(10) unsigned NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_usuario_unique` (`usuario`),
+  KEY `users_idrol_foreign` (`idrol`),
+  CONSTRAINT `users_idrol_foreign` FOREIGN KEY (`idrol`) REFERENCES `roles` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Daniel Velasquez','daniel.vel','$2y$10$x0fKY0GVRNtqxSoBcs/E5OxX/XrsbuRNYpgxPpNWxeDlGy7xnVm7C',1,0.00,0.00,1,'4ngTKskJNk9zNdMH1GICbRwGooRLTHz0u87cOXhQxVf5CqqJaOM3hEgNAPhO','2020-05-24 06:00:00','2020-05-24 06:00:00');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-05-24 21:22:36
